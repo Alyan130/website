2a5a3b7aca9b2dd375f11bc25fcc973174fb0e81
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-09-23 19:50:18 +0500

    web

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,10 @@
-import AboutUs from "../components/about";
+
 import Header from "../components/header";
 
  export default function About(){
     return(
         <>
      <Header/>
-     <AboutUs/>
      </>
     );
 }
--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,5 +1,5 @@
 
-export default function(){
+export default function AboutUs(){
     return(
     <>
 <section className="about-section">
